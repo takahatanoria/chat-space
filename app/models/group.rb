b40f5622407a4b67b4bd8d934b33fd88c974aeb0
takahata-noria <takahatanoria@gmail.com>
2019-10-08 16:43:40 +0900

    message.fin

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
 
+
   def show_last_message
     if (last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
@@ -12,4 +13,7 @@ class Group < ApplicationRecord
       'まだメッセージはありません。'
     end
   end
+
+  # グループにネストしてるメッセージだからさらにアソシエーションあるからできる
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,7 @@ class Message < ApplicationRecord
 
   validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
+
+
 end
 
