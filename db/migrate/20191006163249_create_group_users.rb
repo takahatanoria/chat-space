c170dc7c62a2db9c061f17acf1a546d5ef87d4c9
takahata-noria <takahatanoria@gmail.com>
2019-10-16 20:21:26 +0900

    fix_migration

--- db/migrate/20191006163249_create_group_users.rb
+++ db/migrate/20191006163249_create_group_users.rb
@@ -1,4 +1,4 @@
-class CreateGroupUsers < ActiveRecord::Migration[5.0]
+class CreateGroupUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :group_users do |t|
       t.references :group, foreign_key: true
